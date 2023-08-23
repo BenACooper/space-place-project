5ddf3c61e9feb4b0a1e27f6b7960c60c983735f2
Jatan Jeyachandren <jatanjey@gmail.com>
2023-08-23 13:24:28 -0400

    toggle dark mode

---
+++ Develop/script.js
@@ -0,0 +1,38 @@
+//* Review docs : https://github.com/nasa/apod-api#docs for help
+
+// * This will listen for a submit button click to search APOD using given date
+const btnInput = document.getElementById("submit-button");
+btnInput.addEventListener("click", getAPOD);
+
+const modeToggle = document.getElementById("mode-toggle");
+modeToggle.addEventListener("click", toggleMode);
+
+function toggleMode() {
+    document.body.classList.toggle("light-mode");
+    document.body.classList.toggle("dark-mode");
+}
+
+function getAPOD() {
+    // TODO: Need to generate API key from nasa website
+    // TODO: Create a modal with calendar to select year/date: needs to be string
+    //* Format of date needs to be "A string in YYYY-MM-DD -docs"
+    var dateInput = document.getElementById('date');
+    var dateValue = dateInput.value;
+
+
+
+    var APIKey = "KWP7hL5CquZLoMNheN4c7PgY4gcRxdp3w9cddb2S";
+    var spaceQuery = "https://api.nasa.gov/planetary/apod?api_key=" + APIKey + "&date=" +dateValue;
+
+    var description = document.querySelector("#description");
+    const imageElement = document.getElementById('image');
+
+    fetch(spaceQuery)
+    .then((response) => response.json())
+    .then((data) => {
+
+        description.textContent = data.explanation;
+        imageElement.src = data.hdurl;
+
+    })
+} 
