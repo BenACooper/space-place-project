0ce36d634867057665eef2eb99b74d67770bc48b
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-08-22 20:24:06 -0400

    Added rough APOD function for description and image data

---
+++ Develop/script.js
@@ -0,0 +1,29 @@
+//* Review docs : https://github.com/nasa/apod-api#docs for help
+
+// * This will listen for a submit button click to search APOD using given date
+const btnInput = document.getElementById("submit-button");
+btnInput.addEventListener("click", getAPOD);
+
+function getAPOD() {
+    // TODO: Need to generate API key from nasa website
+    // TODO: Create a modal with calendar to select year/date: needs to be string
+    //* Format of date needs to be "A string in YYYY-MM-DD -docs"
+    
+    var APIKey = "";
+    var dateEl = document.getElementById('date-input').value;
+    var spaceQuery = "https://api.nasa.gov/planetary/apod?api_key=" + APIKey + "&date=" + dateEl;
+
+    var description = document.querySelector(".classForDescriptionElement");
+    var apodImage = document.querySelector(".classForImageElement");
+    
+    fetch(spaceQuery)
+    .then((response) => response.json())
+    .then((data) => {
+
+        description.textContent = data.explination;
+        apodImage.textContent = data.url;
+    })
+} 
+
+
+
