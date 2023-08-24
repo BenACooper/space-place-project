a9b64a7316781d1ef0377725a0934af0eb67f728
jatanjey <141033538+jatanjey@users.noreply.github.com>
2023-08-24 13:44:48 -0400

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,23 @@
-// * This loads the modal function on window load
-window.onload = function() {
-    // * These variables used to manipulate modal and modal content
-    var modal = document.getElementById('modal');
-    var btn = document.getElementById("openModalBtn");
-    var span = document.getElementsByClassName("close")[0];
+// This loads the modal function on window load
+window.onload = function () {
+  var modal = document.getElementById("modal");
+  var btn = document.getElementById("openModalBtn");
+  var span = document.getElementsByClassName("close")[0];
 
-    // * On modal button click, display style block
-    btn.onclick = function() {
-        modal.style.display = "block";
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
     }
-    // * On span click, hide the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-}
+  };
+};
 
-//* Review docs : https://github.com/nasa/apod-api#docs for help
-
-// * This will listen for a submit button click to search APOD using given date
-//! Nasa images library section
+// Review docs: https://github.com/nasa/apod-api#docs for help
 
 const btnInput = document.getElementById("submit-button");
 btnInput.addEventListener("click", getAPOD);
@@ -32,21 +26,15 @@ const modeToggle = document.getElementById("mode-toggle");
 modeToggle.addEventListener("click", toggleMode);
 
 function toggleMode() {
-    document.body.classList.toggle("light-mode");
-    document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("light-mode");
+  document.body.classList.toggle("dark-mode");
 }
 
 function getAPOD() {
-    //* This hides the modal after submit button click event
-    document.getElementById('modal').style.display = "none";
+  document.getElementById("modal").style.display = "none";
 
-    // TODO: Need to generate API key from nasa website
-    // TODO: Create a modal with calendar to select year/date: needs to be string
-    //* Format of date needs to be "A string in YYYY-MM-DD -docs"
-    var dateInput = document.getElementById('date');
-    var dateValue = dateInput.value;
-
-
+  var dateInput = document.getElementById("date");
+  var dateValue = dateInput.value;
 
   var APIKey = "KWP7hL5CquZLoMNheN4c7PgY4gcRxdp3w9cddb2S";
   var spaceQuery =
@@ -55,34 +43,63 @@ function getAPOD() {
     "&date=" +
     dateValue;
 
-    var titleEl = document.querySelector("#apod-title");
-    var descriptionEl = document.querySelector("#description");
-    const imageEl = document.getElementById("image");
+  var titleEl = document.querySelector("#apod-title");
+  var descriptionEl = document.querySelector("#description");
+  const imageEl = document.getElementById("image");
 
   fetch(spaceQuery)
     .then((response) => response.json())
     .then((data) => {
-        titleEl.textContent = data.title;
+      titleEl.textContent = data.title;
       descriptionEl.textContent = data.explanation;
       imageEl.src = data.hdurl;
 
+      saveToLocalStorage(dateValue, data.title);
+      displayHistory();
     });
 }
 
+function saveToLocalStorage(date, title) {
+  const history = JSON.parse(localStorage.getItem("apodHistory")) || [];
 
-//! Nasa images library section
-//NASA IMAGES LIBRARY API
-//API KEY RUp9mWS16zvh33le8ZSYs6OVOzQ0F1KrdbgeEa4z
-//Documentation: https://images.nasa.gov/docs/images.nasa.gov_api_docs.pdf
+  if (history.length >= 5) {
+    history.shift();
+  }
 
+  history.push({ date, title });
+  localStorage.setItem("apodHistory", JSON.stringify(history));
+}
+
+function displayHistory() {
+  const history = JSON.parse(localStorage.getItem("apodHistory")) || [];
+  const historyContainer = document.getElementById("history-container");
+  historyContainer.innerHTML = "";
+
+  const startIdx = Math.max(0, history.length - 5);
+  const recentHistory = history.slice(startIdx);
+
+  recentHistory.forEach((entry) => {
+    const historyItem = document.createElement("div");
+    historyItem.classList.add("history-item");
+
+    const dateEl = document.createElement("p");
+    dateEl.textContent = `Date: ${entry.date}`;
+
+    const titleEl = document.createElement("p");
+    titleEl.textContent = `Title: ${entry.title}`;
+
+    historyItem.appendChild(dateEl);
+    historyItem.appendChild(titleEl);
+
+    historyContainer.appendChild(historyItem);
+  });
+}
+
+// NASA images library section
 var imagesLibraryContainer = document.querySelector(".imagesLibraryContainer");
-
 var celestialArr = ["Horsehead Nebula visible", "Tarantula Nebula Spitzer"];
-
 var galaxyArr = ["M31 Galaxy", "M33 Galaxy"];
-
 var planetMapsArr = ["map of venus", "map of mars"];
-
 var surfacePhotosArr = [
   "surface of pluto",
   "surface of mercury",
@@ -93,9 +110,7 @@ var gameContainerEl = document.querySelector(".gameContainer");
 var gameButtonEl = document.querySelector(".gameButton");
 gameButtonEl.addEventListener("click", selectKeyword);
 
-//Declare function to select a random keyword from one of the arrays.
 function selectKeyword() {
-  //Each nested array will display a distinct question format on the quiz.
   var combinedArray = [
     celestialArr,
     galaxyArr,
@@ -103,26 +118,18 @@ function selectKeyword() {
     surfacePhotosArr,
   ];
 
-  //Select random nested array.
   var randomSubArrayIndex = Math.floor(Math.random() * combinedArray.length);
   var randomSubArray = combinedArray[randomSubArrayIndex];
-  console.log(randomSubArray);
 
-  //Select a random prroperty from the nested array.
   var randomPropertyIndex = Math.floor(Math.random() * randomSubArray.length);
   var randomProperty = randomSubArray[randomPropertyIndex];
-  console.log(randomProperty);
 
-  //Pass proprty to searchLibraryAPI function for use as keyword.
   searchLibraryAPI(randomProperty);
-  // selectQuestion(randomSubArray);
 }
 
-//Declare function that receives randomlly selected keyword to query the image library API and save the response it to localstorage.
 function searchLibraryAPI(selectedKeyword) {
   var queryLibraryURL =
     "https://images-api.nasa.gov/search?q=" + selectedKeyword;
-  console.log(queryLibraryURL);
 
   fetch(queryLibraryURL)
     .then(function (response) {
@@ -137,14 +144,11 @@ function searchLibraryAPI(selectedKeyword) {
     });
 }
 
-//Declare a function that retrieves the data from local storage, parses it, target the necessary property(image URL) to store in a variable.
 function getLibraryData() {
   var rawLibraryData = localStorage.getItem("rawLibraryData");
-  console.log(rawLibraryData);
 
   if (rawLibraryData) {
     var parsedLibraryData = JSON.parse(rawLibraryData);
-    console.log(parsedLibraryData);
 
     var imageLink = parsedLibraryData.collection.items[0].links[0].href;
 
@@ -152,8 +156,7 @@ function getLibraryData() {
   }
 }
 
-//Receives the image URL and displays it.
-var gameImageContainerEl = document.querySelector(".gameImageContainer")
+var gameImageContainerEl = document.querySelector(".gameImageContainer");
 
 function displayLibraryData(imageLink) {
   var existingImageEl = document.getElementById("gameImage");
